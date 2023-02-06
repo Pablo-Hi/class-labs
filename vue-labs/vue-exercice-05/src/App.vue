bb28c884092031e5bcdb24cb7fb7bf8e8c5e12b2
Pablo <pablopenyamulero@gmail.com>
2023-02-06 17:51:21 +0100

    vue updates

---
+++ vue-labs/vue-exercice-05/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import { ref, reactive, computed } from "vue";
+// let firstName = ref("Pablo");
+// let lastName = ref("Peña");
+
+let myName = computed(() => {
+  return `${firstName.value}  ${lastName.value}`;
+});
+
+let myWatchList = reactive([
+  {
+    tittle: "The last of Us";
+    year: 2023,
+    whereToWatch: "HBO",
+  },
+  {
+    tittle: "Vikings",
+    year: 2019,
+    whereToWatch: "Netflix",
+  },
+  {
+    tittle: "Silicon Valley",
+    year: 2020,
+    whereToWatch: "Amazon Prime Video"
+  }
+])
+</script>
+
+<template>
+  <header>
+    <!-- <h1>{{ myName }}</h1>
+    <form action="">
+      <input v-model="firstName" type="text" placeholder="Name" />
+      <input v-model="lastName" type="text" placeholder="Last Name" />
+    </form> -->
+    <h1></h1>
+    <h2></h2>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  color: white;
+}
+</style>
